c9aa616c7d9a3b61ae3a59f88324e8a03f7810bb
pidamupidah <2411102441045@umkt.ac.id>
2026-08-06 14:36:36 +0800

    Rename 'nama' column to 'name' in users table

--- smkn1sbl/smkn1sbl/database/migrations/0001_01_01_000000_create_users_table.php
+++ smkn1sbl/smkn1sbl/database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->foreignId('role_id')->constrained('roles')
                     ->cascadeOnUpdate()
                     ->restrictOnDelete();
-            $table->string('nama', 100);
+            $table->string('name', 100);
             $table->string('email', 100)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
