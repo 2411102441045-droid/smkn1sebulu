d5e7ebded271dc4bcb78387dfbe740e946cc982e
pidamupidah <2411102441045@umkt.ac.id>
2026-08-10 10:09:43 +0800

    Change document_type from string to enum

--- smkn1sbl/smkn1sbl/database/migrations/2024_01_01_000014_create_documents_table.php
+++ smkn1sbl/smkn1sbl/database/migrations/2024_01_01_000014_create_documents_table.php
@@ -11,7 +11,15 @@ return new class extends Migration
         Schema::create('documents', function (Blueprint $table) {
             $table->id();
             $table->foreignId('applicant_id')->constrained()->cascadeOnDelete();
-            $table->string('document_type');   // KK, Akte, Ijazah, Foto, dll
+            $table->enum('document_type', [
+                         'kk',
+                         'akta kelahiran',
+                         'ijazah',
+                         'skl',
+                         'pas foto',
+                         'rapor',
+                         'kip',
+                         ]);
             $table->string('file_path');
             $table->timestamps();
         });
